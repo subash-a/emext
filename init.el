1b73a832b76a96fe408c45b9da386ee14929510a
subash <subash@subash-desktop.(none)>
2014-07-05 01:56:30 +0530

    Added Initialization file for emacs customizations

---
+++ init.el
@@ -0,0 +1,14 @@
+;; creating custom startup file
+(custom-set-variables
+'(column-number-mode t)
+'(global-linum-mode 1)
+'(fill-column 80)
+'(tool-bar-mode nil)
+'(menu-bar-mode nil))
+
+
+
+
+
+
+
