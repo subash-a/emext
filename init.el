b2da419e8501c8f9b1fcef40fd35b76be843ecec
Subhash Sharma <subhash@modelogiq.com>
2015-06-03 14:34:22 +0530

    added iBuffer support for C-x C-b
    Added Ibuffer support

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  
+
   ;; Adding New package installation archives
 (require `package)
 (add-to-list `package-archives
@@ -85,4 +85,5 @@
 (global-set-key (kbd "SPC") 'just-one-space)
 ;; Setting up key bindings for Ace Jump Mode
 (global-set-key (kbd "C-x j") 'ace-jump-mode)
-
+;; Setting up CxCb to bind to ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
